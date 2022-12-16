1e93dcd3e18af782e9ad7913a781def564b98ec4
Fazal Haroon <kharoon187@gmail.com>
2022-12-16 21:17:48 +0400

    best way to create a class todo and what is pipe

--- BasicAngular/src/app/list-todos/list-todos.component.ts
+++ BasicAngular/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+export class Todo {
+  constructor(public id:number, public description: string, public done: boolean, public targetDate: Date) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +15,12 @@ import { Component } from '@angular/core';
 export class ListTodosComponent {
 
   todos = [
-    {id : 1, description: "Learn to code in angular"},
-    {id : 2, description: "Learn to code in reactjs"},
-    {id : 3, description: "Learn to code in frontend"}
+      new Todo(1, "angular", false, new Date()),
+    new Todo(2, "reactjs", false, new Date()),
+    new Todo(3, "frontend", false, new Date())
+    // {id : 1, description: "Learn to code in angular"},
+    // {id : 2, description: "Learn to code in reactjs"},
+    // {id : 3, description: "Learn to code in frontend"}
   ]
 
   todo = {
