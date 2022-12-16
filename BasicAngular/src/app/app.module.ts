3b3548ee0e5a4f30f645202be2741d32082d1b27
Fazal Haroon <kharoon187@gmail.com>
2022-12-16 14:43:15 +0400

    ng generate component login added

--- BasicAngular/src/app/app.module.ts
+++ BasicAngular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
