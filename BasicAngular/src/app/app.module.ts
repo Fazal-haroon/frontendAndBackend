bda23c99db6ca7de089e762ccd803c8797404dca
Fazal Haroon <kharoon187@gmail.com>
2022-12-16 16:41:10 +0400

    routing added for login and welcome pages

--- BasicAngular/src/app/app.module.ts
+++ BasicAngular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-      FormsModule
+      FormsModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BasicAngular/src/app/login/login.component.ts
+++ BasicAngular/src/app/login/login.component.ts
@@ -8,10 +8,19 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   username : string = 'username1'
   password : string = 'password1'
+  errorMessage : string = 'Invalid Credentials Msg'
+  invalidLogin : boolean = false
 
   eventBindingOfLoginOrHandleLogin(){
     console.log(this.username);
-    // console.log("login success matach")
+    if (this.username === "username1" && this.password === "password1") {
+    console.log("login success match")
+      this.invalidLogin = false
+      console.log(this.invalidLogin)
+    } else {
+      this.invalidLogin = true
+      console.log(this.invalidLogin)
+    }
     // console.log(this.password)
     // console.log("password also match")
   }
