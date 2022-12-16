e8588567660e0bced911fdf30f30f429633ad88c
Fazal Haroon <kharoon187@gmail.com>
2022-12-16 15:20:33 +0400

    we can use interpolation to get the value of username and password, and handle login of Event Binding

--- BasicAngular/src/app/login/login.component.ts
+++ BasicAngular/src/app/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username : string = 'username1'
+  password : string = 'pass1'
 
+  eventBindingOfLoginOrHandleLogin(){
+    console.log(this.username);
+    console.log("login success matach")
+    console.log(this.password)
+    console.log("password also match")
+  }
 }
