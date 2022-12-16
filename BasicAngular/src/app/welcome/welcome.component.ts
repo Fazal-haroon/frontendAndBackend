8ba8decbdb588bc226b6bf00049b0d6c146a69e1
Fazal Haroon <kharoon187@gmail.com>
2022-12-16 14:16:57 +0400

    Typescript compare with spring boot Java

--- BasicAngular/src/app/welcome/welcome.component.ts
+++ BasicAngular/src/app/welcome/welcome.component.ts
@@ -1,10 +1,15 @@
+//import org.springframework.boot.SpringApplication;
 import { Component } from '@angular/core';
 
+//@ComponentScan(value = "com.example.springboot.app")
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+//public class SpringBootApplication {
 export class WelcomeComponent {
 
 }
+// }
