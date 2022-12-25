5e8e7dda9c4723ca249e41bbf617753cabbe409d
Fazal Haroon <kharoon187@gmail.com>
2022-12-25 16:14:15 +0400

    JWT Configuration and in memory H2 Database

---
+++ angularBackend/angularBackend/src/main/java/com/example/angularBackend/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.example.angularBackend.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
